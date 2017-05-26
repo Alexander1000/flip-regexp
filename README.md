402bd3ab8fff54bee7ec6406ddcfd9437866bdff
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2017-05-27 00:05:53 +0300

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # flip-regexp
+
+tests:
+```shell
+go test -v -cover .
+```
+
+usage:
+```shell
+package main
+
+import (
+    "flip-regexp"
+    "fmt"
+)
+
+func main() {
+    var str string
+    fmt.Println("Hello world")
+
+    flip := flip_regexp.NewBuilder("[0-9]{5,9}")
+
+    str, err := flip.Render()
+
+    if err != nil {
+        return -1
+    }
+
+    fmt.Printf("String: %s", str)
+    return 0
+}
+
+```
---
+++ main_test.go
@@ -0,0 +1,90 @@
+package flip_regexp
+
+import (
+	"regexp"
+	"testing"
+)
+
+func TestRegexp_Success(t *testing.T) {
+	pattern := "[a-z]{5,6}"
+	reg := NewBuilder(pattern)
+	result, _ := reg.Render()
+
+	re := regexp.MustCompile(pattern)
+
+	if !re.MatchString(result) {
+		t.Fatalf("Excpeted success, false given for pattern: %s; string: %s", pattern, result)
+	} else {
+		t.Logf("Generated string: %s", result)
+	}
+}
+
+func TestRegexp_Complex_Success(t *testing.T) {
+	pattern := "[a-z]{5,6} test [a-zA-Z]{9}"
+	reg := NewBuilder(pattern)
+	result, _ := reg.Render()
+
+	re := regexp.MustCompile(pattern)
+
+	if !re.MatchString(result) {
+		t.Fatalf("Excpeted success, false given for pattern: %s; string: %s", pattern, result)
+	} else {
+		t.Logf("Generated string: %s", result)
+	}
+}
+
+func TestRegexp_PhoneComplex_Success(t *testing.T) {
+	pattern := `\+7 \([489][0-9]{2}\) [0-9]{3} [0-9]{2} [0-9]{2}`
+	reg := NewBuilder(pattern)
+	result, _ := reg.Render()
+
+	re := regexp.MustCompile(pattern)
+
+	if !re.MatchString(result) {
+		t.Fatalf("Excpeted success, false given for pattern: %s; string: %s", pattern, result)
+	} else {
+		t.Logf("Generated string: %s", result)
+	}
+}
+
+func TestRegexp_EscapeComplexPattern_Success(t *testing.T) {
+	pattern := `\+7 \(\[489\][0-9]{2}\) [0-9]{3} \[0-9\]\{2\} [0-9]{2}`
+	reg := NewBuilder(pattern)
+	result, _ := reg.Render()
+
+	re := regexp.MustCompile(pattern)
+
+	if !re.MatchString(result) {
+		t.Fatalf("Excpeted success, false given for pattern: %s; string: %s", pattern, result)
+	} else {
+		t.Logf("Generated string: %s", result)
+	}
+}
+
+func TestRegexp_OrWithPipePattern_Success(t *testing.T) {
+	pattern := `(status|very|important)`
+	reg := NewBuilder(pattern)
+	result, _ := reg.Render()
+
+	re := regexp.MustCompile(pattern)
+
+	if !re.MatchString(result) {
+		t.Fatalf("Excpeted success, false given for pattern: %s; string: %s", pattern, result)
+	} else {
+		t.Logf("Generated string: %s", result)
+	}
+}
+
+func TestRegexp_OrWithPipeAndQuestionPattern_Success(t *testing.T) {
+	pattern := `hello (status|very|important) and (man|woman)? test`
+	reg := NewBuilder(pattern)
+	result, _ := reg.Render()
+
+	re := regexp.MustCompile(pattern)
+
+	if !re.MatchString(result) {
+		t.Fatalf("Excpeted success, false given for pattern: %s; string: %s", pattern, result)
+	} else {
+		t.Logf("Generated string: %s", result)
+	}
+}
