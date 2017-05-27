31a3f48545a42c3926bca983e46beb3cef12d602
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2017-05-27 11:14:07 +0300

    extend tests

--- main_test.go
+++ main_test.go
@@ -7,84 +7,120 @@ import (
 
 func TestRegexp_Success(t *testing.T) {
 	pattern := "[a-z]{5,6}"
-	reg := NewBuilder([]byte(pattern))
-	result, _ := reg.Render()
+	i := 0
 
-	re := regexp.MustCompile(pattern)
+	for i < 10 {
+		reg := NewBuilder([]byte(pattern))
+		result, _ := reg.Render()
 
-	if !re.MatchString(string(result)) {
-		t.Fatalf("Excpeted success, false given for pattern: %s; string: %s", pattern, result)
-	} else {
-		t.Logf("Generated string: %s", result)
+		re := regexp.MustCompile(pattern)
+
+		if !re.MatchString(string(result)) {
+			t.Fatalf("Excpeted success, false given for pattern: %s; string: %s", pattern, result)
+		} else {
+			t.Logf("Generated string: %s", result)
+		}
+
+		i++
 	}
 }
 
 func TestRegexp_Complex_Success(t *testing.T) {
 	pattern := "[a-z]{5,6} test [a-zA-Z]{9}"
-	reg := NewBuilder([]byte(pattern))
-	result, _ := reg.Render()
+	i := 0
 
-	re := regexp.MustCompile(pattern)
+	for i < 10 {
+		reg := NewBuilder([]byte(pattern))
+		result, _ := reg.Render()
 
-	if !re.MatchString(string(result)) {
-		t.Fatalf("Excpeted success, false given for pattern: %s; string: %s", pattern, result)
-	} else {
-		t.Logf("Generated string: %s", result)
+		re := regexp.MustCompile(pattern)
+
+		if !re.MatchString(string(result)) {
+			t.Fatalf("Excpeted success, false given for pattern: %s; string: %s", pattern, result)
+		} else {
+			t.Logf("Generated string: %s", result)
+		}
+
+		i++
 	}
 }
 
 func TestRegexp_PhoneComplex_Success(t *testing.T) {
 	pattern := `\+7 \([489][0-9]{2}\) [0-9]{3} [0-9]{2} [0-9]{2}`
-	reg := NewBuilder([]byte(pattern))
-	result, _ := reg.Render()
+	i := 0
 
-	re := regexp.MustCompile(pattern)
+	for i < 10 {
+		reg := NewBuilder([]byte(pattern))
+		result, _ := reg.Render()
 
-	if !re.MatchString(string(result)) {
-		t.Fatalf("Excpeted success, false given for pattern: %s; string: %s", pattern, result)
-	} else {
-		t.Logf("Generated string: %s", result)
+		re := regexp.MustCompile(pattern)
+
+		if !re.MatchString(string(result)) {
+			t.Fatalf("Excpeted success, false given for pattern: %s; string: %s", pattern, result)
+		} else {
+			t.Logf("Generated string: %s", result)
+		}
+
+		i++
 	}
 }
 
 func TestRegexp_EscapeComplexPattern_Success(t *testing.T) {
 	pattern := `\+7 \(\[489\][0-9]{2}\) [0-9]{3} \[0-9\]\{2\} [0-9]{2}`
-	reg := NewBuilder([]byte(pattern))
-	result, _ := reg.Render()
+	i := 0
 
-	re := regexp.MustCompile(pattern)
+	for i < 10 {
+		reg := NewBuilder([]byte(pattern))
+		result, _ := reg.Render()
 
-	if !re.MatchString(string(result)) {
-		t.Fatalf("Excpeted success, false given for pattern: %s; string: %s", pattern, result)
-	} else {
-		t.Logf("Generated string: %s", result)
+		re := regexp.MustCompile(pattern)
+
+		if !re.MatchString(string(result)) {
+			t.Fatalf("Excpeted success, false given for pattern: %s; string: %s", pattern, result)
+		} else {
+			t.Logf("Generated string: %s", result)
+		}
+
+		i++
 	}
 }
 
 func TestRegexp_OrWithPipePattern_Success(t *testing.T) {
 	pattern := `(status|very|important)`
-	reg := NewBuilder([]byte(pattern))
-	result, _ := reg.Render()
+	i := 0
 
-	re := regexp.MustCompile(pattern)
+	for i < 10 {
+		reg := NewBuilder([]byte(pattern))
+		result, _ := reg.Render()
 
-	if !re.MatchString(string(result)) {
-		t.Fatalf("Excpeted success, false given for pattern: %s; string: %s", pattern, result)
-	} else {
-		t.Logf("Generated string: %s", result)
+		re := regexp.MustCompile(pattern)
+
+		if !re.MatchString(string(result)) {
+			t.Fatalf("Excpeted success, false given for pattern: %s; string: %s", pattern, result)
+		} else {
+			t.Logf("Generated string: %s", result)
+		}
+
+		i++
 	}
 }
 
 func TestRegexp_OrWithPipeAndQuestionPattern_Success(t *testing.T) {
 	pattern := `hello (status|very|important) and (man|woman)? test`
-	reg := NewBuilder([]byte(pattern))
-	result, _ := reg.Render()
+	i := 0
 
-	re := regexp.MustCompile(pattern)
+	for i < 10 {
+		reg := NewBuilder([]byte(pattern))
+		result, _ := reg.Render()
 
-	if !re.MatchString(string(result)) {
-		t.Fatalf("Excpeted success, false given for pattern: %s; string: %s", pattern, result)
-	} else {
-		t.Logf("Generated string: %s", result)
+		re := regexp.MustCompile(pattern)
+
+		if !re.MatchString(string(result)) {
+			t.Fatalf("Excpeted success, false given for pattern: %s; string: %s", pattern, result)
+		} else {
+			t.Logf("Generated string: %s", result)
+		}
+
+		i++
 	}
 }
